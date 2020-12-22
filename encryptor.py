675254d38303a9ba1ba37f0285b659b3dc4b3b76
vomiz9k <kirill@kyazimov.ru>
2020-12-22 05:03:43 +0300

    fixed zamechaniya

--- encryptor.py
+++ encryptor.py
@@ -2,18 +2,19 @@
 import sys
 import os
 import pickle
+import argparse
 
 upper_alph = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 lower_alph = 'abcdefghijklmnopqrstuvwxyz'
 
 
 def encoded_caesar(text, key):
-    new_text = list()
+    new_text = []
     for letter in text:
         if letter.isupper():
-            new_text.append(upper_alph[(upper_alph.find(letter) + key) % 26])
+            new_text.append(upper_alph[(upper_alph.find(letter) + key) % len(lower_alph)])
         elif letter.islower():
-            new_text.append(lower_alph[(lower_alph.find(letter) + key) % 26])
+            new_text.append(lower_alph[(lower_alph.find(letter) + key) % len(lower_alph)])
         else:
             new_text.append(letter)
     return ''.join(new_text)
@@ -28,54 +29,64 @@ for i in range(len(lower_alph)):
         vigenere_table[lower_alph[i]][lower_alph[j]] = curr_alph[j]
 
 
-def in_out_decorator(func):
-    def wrapped(args):
-
-        encoded = func(args)
-        put_data(args, encoded)
-        return encoded
-    return wrapped
 
 
 def parse_argv():
-    data = dict()
-    data['mode'] = sys.argv[1]
-    for i in range(2, len(sys.argv)):
-        if sys.argv[i][:2] == '--':
-            data[sys.argv[i][2:]] = sys.argv[i + 1]
-            i += 1
-    return data
+    parser = argparse.ArgumentParser(description='Working with some cifers.')
+    parser.add_argument('--input-file', action='store', type=argparse.FileType('r'), required=False, help='input file')
+    parser.add_argument('--output-file', action='store', type=argparse.FileType('w'), required=False, help='output file')
+    
+    subparsers = parser.add_subparsers(title='mode', dest='mode', help='mode')
+
+    encoder = subparsers.add_parser('encode', help='encode')
+    encoder.add_argument('--cipher', action='store', type=str, required=True, help='cipher')
+    encoder.add_argument('--key', action='store', type=str, required=True, help='key')
+
+    decoder = subparsers.add_parser('decode', help='decode')
+    decoder.add_argument('--cipher', action='store', type=str, required=True, help='cipher')
+    decoder.add_argument('--key', action='store', type=str, required=True, help='key')
+    
+    trainer = subparsers.add_parser('train', help='train')
+    trainer.add_argument('--model-file', action='store', type=str, required=True, help='model file')
+
+    hacker= subparsers.add_parser('hack', help='hack')
+    subhackparsers = hacker.add_subparsers(title='method', dest='method', help='hacking method')
+
+    base_hacker = subhackparsers.add_parser('base', help='hacking by base file')
+    base_hacker.add_argument('--base-file', action='store', type=argparse.FileType('r'), required=True, help='base file')
+
+    model_hacker = subhackparsers.add_parser('model', help='hacking by model file')
+    model_hacker.add_argument('--model-file', action='store', type=argparse.FileType('rb'), required=True, help='model file')
+
+    
+    return parser.parse_args()
 
 
-def get_data(args):
+
+def get_data(input_file):
     data = str()
-    if 'input-file' in args.keys():
-        with open(args['input-file'], 'r') as file:
-            data = file.read()
-    if "input-file" in args.keys():
-        file = open(args["input-file"], "r")
-        data = file.read()
-        file.close()
-    else:
+    if input_file is None:
         data = sys.stdin.read()
+    else:
+        data = input_file.read()
     return data
 
 
-def put_data(args, data):
-    if 'output-file' in args.keys():
-        with open(args['output-file'], 'w') as file:
-            file.write(data)
-    else:
+def put_data(data, output_file):
+    if output_file is None:
         print(data)
+    else:
+        output_file.write(data)
+        
 
 
 def encoded_vigenere(data, keyword):
-    key = list()
+    key = []
 
     for i in range(len(data)):
         key.append(keyword[i % len(keyword)])
     key = ''.join(key)
-    encoded = list()
+    encoded = []
 
     for i in range(len(data)):
         if data[i].islower():
@@ -89,12 +100,12 @@ def encoded_vigenere(data, keyword):
 
 
 def decoded_vigenere(data, keyword):
-    key = list()
+    key = []
 
     for i in range(len(data)):
         key.append(keyword[i % len(keyword)])
     key = ''.join(key)
-    encoded = list()
+    encoded = []
 
     for i in range(len(data)):
         curr_str = vigenere_table[key[i]]
@@ -113,19 +124,20 @@ def decoded_vigenere(data, keyword):
 
     
 
-def hack_by_base(data, args):
-    with open(args['base']) as base:
-        words = set()
-        for line in base:
-            words.add(line)
+def hack_by_base(input_file, output_file, base_file):
+    data = get_data(input_file)
+    words = set()
+    for line in base_file:
+        words.add(line[:-1])
 
     best_match = 0
     best_key = 0
 
-    for key in range(26):
+    for key in range(len(lower_alph)):
         curr_match = 0
         curr_data = encoded_caesar(data, key)
         curr_data_list = curr_data.split()
+        print(curr_data_list)
         for el in curr_data_list:
             if el in words:
                 curr_match += 1
@@ -134,18 +146,18 @@ def hack_by_base(data, args):
             best_match = curr_match
             best_key = key
 
-    return encoded_caesar(data, best_key)
+    put_data(encoded_caesar(data, best_key), output_file)
 
 
-def hack_by_model(data, args):
-    with open(args['model-file'], 'rb') as model_file:
-        values = pickle.load(model_file)
+def hack_by_model(input_file, output_file, model_file):
+    data = get_data(input_file)
+    values = pickle.load(model_file)
 
     model_frequency = get_frequency(values)
     best_diff = 3333210937801733.0  # very big number
     best_key = 0
 
-    for k in range(26):
+    for k in range(len(lower_alph)):
         curr_values = dict()
         curr_values['count'] = 0
         for char in lower_alph:
@@ -168,7 +180,7 @@ def hack_by_model(data, args):
             best_diff = curr_diff
             best_key = k
 
-    return encoded(data, best_key)
+    put_data(encoded_caesar(data, best_key), output_file)
 
 
 def get_frequency(values):
@@ -180,36 +192,33 @@ def get_frequency(values):
     return frequency
 
 
-def encode(args):
-    data = get_data(args)
-    if args['cipher'] == 'caesar':
-        key = int(args['key']) % 26
-        encoded = encoded_caesar(data, key)
-    elif args['cipher'] == 'vigenere':
-        keyword = args['key']
-        encoded = encoded_vigenere(data, keyword)
-    put_data(args, encoded)
+def encode(cipher, key, input_file, output_file):
+    data = get_data(input_file)
+    if cipher == 'caesar':
+        encoded = encoded_caesar(data, int(key))
+    elif cipher == 'vigenere':
+        encoded = encoded_vigenere(data, key)
+    put_data(encoded, output_file)
 
 
-def decode(args):
-    data = get_data(args)
-    if args['cipher'] == 'caesar':
-        key = (26 - int(args['key'])) % 26
+def decode(cipher, key, input_file, output_file):
+    data = get_data(input_file)
+    if cipher == 'caesar':
+        key = (len(lower_alph) - int(key)) % len(lower_alph)
         decoded = encoded_caesar(data, key)
-    elif args['cipher'] == 'vigenere':
-        keyword = args['key']
-        decoded = decoded_vigenere(data, keyword)
-    put_data(args, decoded)
+    elif args.cipher == 'vigenere':
+        decoded = decoded_vigenere(data, key)
+    put_data(decoded, output_file)
 
 
-def train(args):
-    data = get_data(args)
+def train(input_file, model_file):
+    data = get_data(input_file)
     new_values = dict()
     new_values['count'] = 0
     for char in lower_alph:
         new_values[char] = 0
 
-    with open(args['model-file'], 'rb') as model_file:
+    with open(model_file, 'rb') as model_file:
         try:
             values = dict()
             values = pickle.load(model_file)
@@ -223,32 +232,26 @@ def train(args):
             new_values['count'] += 1
             new_values[letter.lower()] += 1
 
-    with open(args['model-file'], 'wb') as model_file:
+    with open(model_file, 'wb') as model_file:
         pickle.dump(new_values, model_file)
 
 
-def hack(args):
-    data = get_data(args)
-    if 'base' in args.keys():  # --base base.txt means base.txt is file with big amount of words. check words.txt
-        encoded = hack_by_base(data, args)
-    elif 'model-file' in args.keys:
-        encoded = hack_by_model(data, args)
-    else:
-        print('ERROR! Need something to hack. Try --base or --model-file.')
 
-    put_data(args, encoded)
+#main:
 
-
-# main:
 args = parse_argv()
 
-if args['mode'] == 'encode':
-    encode(args)
-elif args['mode'] == 'decode':
-    decode(args)
-elif args['mode'] == 'train':
-    train(args)
-elif args['mode'] == 'hack':
-    hack(args)
+if args.mode == 'encode':
+    encode(args.cipher, args.key, args.input_file, args.output_file)
+elif args.mode == 'decode':
+    decode(args.cipher, args.key, args.input_file, args.output_file)
+elif args.mode == 'train':
+    train(args.input_file, args.model_file)
+elif args.mode == 'hack':
+    if args.method == 'base':
+        print("base")
+        hack_by_base(args.input_file, args.output_file, args.base_file)
+    elif args.method == 'model':
+        hack_by_model(args.input_file, args.output_file, args.model_file)
 else:
     print('INCORRECT MODE')
